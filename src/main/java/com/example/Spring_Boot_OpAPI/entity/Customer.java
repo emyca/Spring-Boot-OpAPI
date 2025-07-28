be8d7a9e8c962b7d5b610756079855dcaf7e0a15
emyca <evmyca@gmail.com>
2025-07-28 09:18:54 +0300

    Add item entity

---
+++ src/main/java/com/example/Spring_Boot_OpAPI/entity/Customer.java
@@ -0,0 +1,25 @@
+package com.example.Spring_Boot_OpAPI.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+}
