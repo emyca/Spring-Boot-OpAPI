0420f3b6beaa5475327e2b08a5335dd9b91d5cd5
emyca <evmyca@gmail.com>
2025-07-29 12:46:42 +0300

    Inject item mapper and repository

--- src/main/java/com/example/Spring_Boot_OpAPI/service/CustomerServiceImpl.java
+++ src/main/java/com/example/Spring_Boot_OpAPI/service/CustomerServiceImpl.java
@@ -2,11 +2,21 @@ package com.example.Spring_Boot_OpAPI.service;
 
 import com.example.Spring_Boot_OpAPI.dto.CustomerDtoRequest;
 import com.example.Spring_Boot_OpAPI.dto.CustomerDtoResponse;
+import com.example.Spring_Boot_OpAPI.dto.CustomerMapper;
+import com.example.Spring_Boot_OpAPI.repository.CustomerRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service("CustomerServiceImpl")
 public class CustomerServiceImpl implements CustomerService {
+
+    private CustomerMapper mapper;
+    private CustomerRepository repository;
+
+    public CustomerServiceImpl(CustomerMapper mapper, CustomerRepository repository) {
+        this.mapper = mapper;
+        this.repository = repository;
+    }
 
     @Override
     public ResponseEntity<CustomerDtoResponse> create(CustomerDtoRequest request) {
