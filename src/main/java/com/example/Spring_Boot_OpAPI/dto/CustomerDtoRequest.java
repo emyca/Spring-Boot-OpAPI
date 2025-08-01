48bc76d13062c146134cad9ecdd14db155ae513d
emyca <evmyca@gmail.com>
2025-08-01 19:52:15 +0300

    Add API docs to request dto model

--- src/main/java/com/example/Spring_Boot_OpAPI/dto/CustomerDtoRequest.java
+++ src/main/java/com/example/Spring_Boot_OpAPI/dto/CustomerDtoRequest.java
@@ -1,11 +1,32 @@
 package com.example.Spring_Boot_OpAPI.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record CustomerDtoRequest(
+        @Schema(
+                name = "Id",
+                example = "1",
+                requiredMode = Schema.RequiredMode.AUTO,
+                description = "Customer object id")
         Long id,
+        @Schema(
+                name = "First name",
+                example = "John",
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                description = "Customer first name")
         String firstName,
+        @Schema(
+                name = "Last name",
+                example = "Doe",
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                description = "Customer last name")
         String lastName,
+        @Schema(
+                name = "Email",
+                example = "john@mail.com",
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                description = "Customer email")
         String email) {
 }
