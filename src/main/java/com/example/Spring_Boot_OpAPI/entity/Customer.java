e110a4098469ac2c95cf1f4b2009b7728c6603a7
emyca <evmyca@gmail.com>
2025-08-01 09:27:59 +0300

    Update item entity model

--- src/main/java/com/example/Spring_Boot_OpAPI/entity/Customer.java
+++ src/main/java/com/example/Spring_Boot_OpAPI/entity/Customer.java
@@ -1,6 +1,9 @@
 package com.example.Spring_Boot_OpAPI.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -16,10 +19,18 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
+    @NotNull
+    @Size(min = 2, max = 24)
     @Column(name = "first_name")
     private String firstName;
+
+    @NotNull
+    @Size(min = 2, max = 48)
     @Column(name = "last_name")
     private String lastName;
+
+    @Email(message = "Email should be valid")
     @Column(name = "email")
     private String email;
 }
