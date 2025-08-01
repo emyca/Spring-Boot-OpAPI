3b77a0f4f4bb6d7963e6823b3dee23986891bbb5
emyca <evmyca@gmail.com>
2025-08-01 20:58:36 +0300

    Update creation API docs with request and response details

--- src/main/java/com/example/Spring_Boot_OpAPI/controller/CustomerController.java
+++ src/main/java/com/example/Spring_Boot_OpAPI/controller/CustomerController.java
@@ -4,6 +4,11 @@ import com.example.Spring_Boot_OpAPI.dto.CustomerDtoRequest;
 import com.example.Spring_Boot_OpAPI.dto.CustomerDtoResponse;
 import com.example.Spring_Boot_OpAPI.service.CustomerService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,8 +26,56 @@ public class CustomerController {
 
     @Operation(
             summary = "Creates new customer",
-            description = "Creates new Customer object. " +
-                    "The response is newly created Customer object.")
+            description = "Creates new Customer object or fails.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "New Customer data to be saved.",
+                    required = true,
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomerDtoRequest.class),
+                            examples = @ExampleObject(
+                                    name = "Customer data to save",
+                                    description = "New Customer data for saving into DB.",
+                                    value = """
+                                            {
+                                            	"firstName": "John",
+                                            	"lastName": "Doe",
+                                            	"email": "john@mail.com"
+                                            }"""))))
+    @ApiResponses({
+            @ApiResponse(responseCode = "201",
+                    description = "Customer creation success",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomerDtoResponse.class),
+                            examples = @ExampleObject(
+                                    name = "Successful Customer creation",
+                                    description = "Customer has been created successfully.",
+                                    value = """
+                                            {
+                                                "status": 201,
+                                                "reasonPhrase": "Created",
+                                                "success": true,
+                                                "message": "Customer has been created successfully.",
+                                                "customer": {
+                                                    "id": 1,
+                                                    "firstName": "John",
+                                                    "lastName": "Doe",
+                                                    "email": "john@mail.com"
+                                                }
+                                            }"""))}),
+            @ApiResponse(responseCode = "204",
+                    description = "Customer creation failure",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomerDtoResponse.class),
+                            examples = @ExampleObject(
+                                    name = "Customer has NOT been created",
+                                    description = "Unsuccessful Customer creation",
+                                    value = """
+                                            {
+                                                "status": 204,
+                                                "reasonPhrase": "No Content",
+                                                "success": false,
+                                                "message": "Customer has NOT been created."
+                                            }""")))})
     @PostMapping
     public ResponseEntity<CustomerDtoResponse> create(
             @RequestBody CustomerDtoRequest request) {
