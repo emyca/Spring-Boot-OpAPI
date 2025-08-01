8232990083d2b20fc5ffe167927513602e0c62c4
emyca <evmyca@gmail.com>
2025-08-01 20:17:32 +0300

    Add API docs to entity model

--- src/main/java/com/example/Spring_Boot_OpAPI/entity/Customer.java
+++ src/main/java/com/example/Spring_Boot_OpAPI/entity/Customer.java
@@ -1,5 +1,6 @@
 package com.example.Spring_Boot_OpAPI.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
@@ -15,21 +16,41 @@ import lombok.*;
 @Table(name = "customers")
 public class Customer {
 
+    @Schema(
+            name = "Id",
+            example = "1",
+            requiredMode = Schema.RequiredMode.AUTO,
+            description = "Customer object id")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
+    @Schema(
+            name = "First name",
+            example = "John",
+            requiredMode = Schema.RequiredMode.REQUIRED,
+            description = "Customer first name")
     @NotNull
     @Size(min = 2, max = 24)
     @Column(name = "first_name")
     private String firstName;
 
+    @Schema(
+            name = "Last name",
+            example = "Doe",
+            requiredMode = Schema.RequiredMode.REQUIRED,
+            description = "Customer last name")
     @NotNull
     @Size(min = 2, max = 48)
     @Column(name = "last_name")
     private String lastName;
 
+    @Schema(
+            name = "Email",
+            example = "john@mail.com",
+            requiredMode = Schema.RequiredMode.REQUIRED,
+            description = "Customer email")
     @Email(message = "Email should be valid")
     @Column(name = "email")
     private String email;
